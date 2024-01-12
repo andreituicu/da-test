2de63f8bfa2f5a6b9bc1313b4c0665b150cf39c4
Andrei Tuicu <tuicu@adobe.com>
2024-01-12 08:19:26 -0800

    add test block

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,11 +1,16 @@
 export default function decorate(block) { 
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('button-container');
+
+
   const anchor = document.createElement('a');
   anchor.textContent = 'Click Me';
   anchor.classList.add('button');
-  anchor.classList.add('primary');
   anchor.addEventListener('click', () => {
     alert('Do you just click on anything?');
   });
 
-  block.append(anchor);
+  paragraph.append(anchor);
+
+  block.append(paragraph);
 }
